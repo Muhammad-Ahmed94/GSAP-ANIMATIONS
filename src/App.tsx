7701b8d4e5fa539b64d5063657217bf83aefa1ec
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-01-08 19:29:07 +0500

    little box animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,37 @@ import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 
 const App = () => {
-  const boxRef = useRef(null);
+  const boxRef1 = useRef(null);
+  const boxRef2= useRef(null);
+  const boxRef3 = useRef(null);
 
   useEffect(() => {
-    gsap.to(boxRef.current, {
-      x: 500,
+    gsap.to(boxRef1.current, {
       duration: 2,
       delay: 1,
-      rotate: 360,
+      rotate: -360,
       backgroundColor: "crimson",
     });
+
+    gsap.to(boxRef2.current, {
+      x: -200,
+      duration: 1.5,
+      delay: 1,
+      backgroundColor: "yellow"
+    });
+
+    gsap.to(boxRef3.current, {
+      x: 300,
+      duration: 3,
+      backgroundColor: "gray",
+    })
   }, []);
 
   return (
-    <div ref={boxRef} className="text-xl h-64 w-64 bg-orange-400 text-white">
-      App
+    <div className="h-screen w-screen uppercase flex flex-col justify-center items-center gap-6">
+      <div ref={boxRef1} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
+      <div ref={boxRef2} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
+      <div ref={boxRef3} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
     </div>
   );
 };

b6ccef43223f06935dc07063378baf81872b5ad4
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-01-12 23:18:23 +0500

    box deleted

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef } from "react";
 const App = () => {
   const boxRef1 = useRef(null);
   const boxRef2= useRef(null);
-  const boxRef3 = useRef(null);
 
   useEffect(() => {
     gsap.to(boxRef1.current, {
@@ -21,20 +20,12 @@ const App = () => {
       backgroundColor: "yellow"
     });
 
-    gsap.to(boxRef3.current, {
-      x: 300,
-      duration: 3,
-      delay: 1,
-      backgroundColor: "gray",
-    });
-
   }, []);
 
   return (
     <div className="h-screen w-screen uppercase flex flex-col justify-center items-center gap-6">
       <div ref={boxRef1} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
       <div ref={boxRef2} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
-      <div ref={boxRef3} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
     </div>
   );
 };

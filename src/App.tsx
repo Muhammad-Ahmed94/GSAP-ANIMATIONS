9cdfa6f4f8bd58ea4da9e794e1c442f3df02e60e
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-01-15 22:05:29 +0500

    new box add

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 
 const App = () => {
   const boxRef1 = useRef(null);
+  const boxRef2 = useRef(null);
 
   useEffect(() => {
     gsap.to(boxRef1.current, {
@@ -12,11 +13,19 @@ const App = () => {
       backgroundColor: "crimson",
     });
 
+    gsap.to(boxRef2.current, {
+      x: 400,
+      duration: 2,
+      delay: 1,
+      backgroundColor: "yellow",
+      borderColor: "red"
+    });
   }, []);
 
   return (
     <div className="h-screen w-screen uppercase flex flex-col justify-center items-center gap-6">
       <div ref={boxRef1} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
+      <div ref={boxRef2} className="w-24 h-24 rounded-md bg-slate-600 text-center"></div>
     </div>
   );
 };
